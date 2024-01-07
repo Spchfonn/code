6e53a7b481457d3c49a028dd10e76665165c911c
Spchfonn <57552344+Spchfonn@users.noreply.github.com>
2024-01-07 22:05:24 +0700

    Add files via upload

---
+++ prob solve/jumppoint.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+int x[1010], y[1010];
+int main(){
+    int n, r;
+    cin >> n >> r;
+    for(int i=0;i<n;i++){
+        cin >> x[i] >> y[i];
+    }
+    
+    return 0;
+}
