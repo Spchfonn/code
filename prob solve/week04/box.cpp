df24869e8e9418732ea3bcde5b7e977a0f2afe26
Spchfonn <57552344+Spchfonn@users.noreply.github.com>
2024-03-12 01:11:03 +0700

    add box

--- prob solve/week04/box.cpp
+++ prob solve/week04/box.cpp
@@ -1,77 +1,42 @@
 #include<bits/stdc++.h>
 using namespace std;
-char arr[33][33], new_arr[33][33];
+char arr[33][33];
+int mark[33][33];
+int n, m;
+pair<int,int> di[] = {{0,1},{0,-1},{1,0},{-1,0}};
 
-bool move_box(int i, int j, int ed){
-    // if(new_arr[i+1][j] == '.') move_box(i+1,j,ed);
-    // if(new_arr[i][j+1] == '.') move_box(i,j+1,ed);
-    // if(new_arr[i][j-1] == '.') move_box(i,j-1,ed);
-    if(i==ed-1) return true;
-    if(new_arr[i+1][j] == '#' && new_arr[i][j+1] == '#' && new_arr[i][j-1] == '#') return false;
-    return move_box(i+1,j,ed) || move_box(i,j+1,ed) || move_box(i,j-1,ed);
+bool is_valid(int i, int j){
+    return (i>=0 && i<n-1 && j>=0 && j<m-1);
+}
+
+void move_box(int i, int j){
+    mark[i][j] = 1;
+    if(i == n-2){
+        cout << "yes\n";
+        exit(0);
+    }
+    for(int k=0;k<4;k++){
+        int nowi = di[k].first + i, nowj = di[k].second + j;
+        if(!is_valid(nowi, nowj) || mark[nowi][nowj]) continue;
+        if(arr[nowi][nowj]=='.' && arr[nowi][nowj+1]=='.' && arr[nowi+1][nowj]=='.' && arr[nowi+1][nowj+1]=='.'){
+            move_box(nowi, nowj);
+        }
+    }
+    mark[i][j] = 0;
 }
 
 int main(){
     ios::sync_with_stdio(0); cin.tie(0);
-    int n, m;
-    bool chk = false;
     cin >> n >> m;
     for(int i=0;i<n;i++){
         for(int j=0;j<m;j++){
             cin >> arr[i][j];
         }
     }
-    cout << "xxx\n";
-
-    for(int i=0;i<n;i++){
-        for(int j=0;j<m;j++){
-            if(arr[i][j+1] == arr[i][j]) new_arr[i][j] = arr[i][j];
-            else new_arr[i][j] = '#';
-        }
-    }
-
-    for(int i=0;i<n;i++){
-        for(int j=0;j<m;j++){
-            cout << new_arr[i][j];
-        }
-        cout << "\n";
-    }
-
     for(int j=0;j<m;j++){
-        if(new_arr[0][j] == '.') chk = move_box(0,j,n);
+        if(arr[0][j]=='.' && arr[1][j]=='.' && arr[0][j+1]=='.' && arr[1][j+1]=='.') move_box(0,j);
     }
-
-    if(chk) cout << "yes\n";
-    else cout << "no\n";
+    cout << "no\n";
 
     return 0;
 }
-
-/*
-7 14
-##..##...#.#..
-##..##...#.#..
-##.####..#.#..
-#...#....#####
-..........###.
-####...######.
-...#..##..###.
-
-7 14
-..............
-..............
-..............
-..............
-..............
-..............
-..............
-
-7 14
-##############
-##############
-##############
-##############
-##############
-##############
-##############
-*/
