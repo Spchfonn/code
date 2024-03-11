2bd1a3255b40366d8d57e5d6bcb9b657e6498ac7
Spchfonn <57552344+Spchfonn@users.noreply.github.com>
2024-03-11 21:58:56 +0700

    add minimal cover

--- prob solve/week05/uva10020_minimalcoverage.cpp
+++ prob solve/week05/uva10020_minimalcoverage.cpp
@@ -1,104 +1,43 @@
-// #include<bits/stdc++.h>
-// using namespace std;
-// struct Pair{
-//     int start, end;
-//     bool operator<(const Pair & other) const{
-//         return start < other.start;
-//     }
-// };
-
-// Pair allPairs[100005];
-// Pair solution[100005];
-
-// int main(){
-//     freopen("input.in","r",stdin);
-//     freopen("output.out","w",stdout);
-//     int t, m;
-//     cin >> t;
-//     while(t--){
-//         cin >> m;
-//         int numPairs = 0;
-//         while (cin >> allPairs[numPairs].start >> allPairs[numPairs].end,
-//             allPairs[numPairs].start || allPairs[numPairs].end)
-//         {
-//             if(allPairs[numPairs].end > 0 && allPairs[numPairs].start < m)
-//                 ++numPairs;
-//         }        
-//         sort(allPairs, allPairs + numPairs);  
-//         allPairs[numPairs].start = m + 1;
-//         int cnt = 0, currentX = 0, currentPair = 0;
-        
-//         while(currentX < m && currentPair < numPairs){
-//             solution[cnt].end = 0;
-//             for(;allPairs[currentPair].start <= currentX;++currentPair)
-//                 if (allPairs[currentPair].end > solution[cnt].end)
-//                     solution[cnt] = allPairs[currentPair];
-            
-//             if (solution[cnt].end == currentX) break;
-//             currentX = solution[cnt].end;
-//             ++cnt;
-//         }
-        
-//         if(currentX >= m){
-//             cout << cnt << endl;
-//             for (int i=0;i<cnt;++i){
-//                 cout << solution[i].start << " " << solution[i].end << endl;
-//             }
-//         }
-//         else cout << "0" << endl;
-//         if(t!=0) cout << "\n";
-//     }
-//     return 0;
-// }
-
 #include<bits/stdc++.h>
 using namespace std;
-struct Pair{
-    int start, end;
-    bool operator<(const Pair & other) const{
-        return start < other.start;
-    }
-};
-
-Pair allPairs[100005];
-Pair solution[100005];
+pair<int,int> all[100010], solution[100010];
 
 int main(){
-    freopen("input.in","r",stdin);
-    freopen("output.out","w",stdout);
-    int t, m;
+    // freopen("input.in","r",stdin);
+    // freopen("output.out","w",stdout);
+    int t, m, l, r;
     cin >> t;
     while(t--){
         cin >> m;
-        int numPairs = 0;
-        while (cin >> allPairs[numPairs].start >> allPairs[numPairs].end,
-            allPairs[numPairs].start || allPairs[numPairs].end)
-        {
-            if(allPairs[numPairs].end > 0 && allPairs[numPairs].start < m)
-                ++numPairs;
-        }        
-        sort(allPairs, allPairs + numPairs);  
-        allPairs[numPairs].start = m + 1;
+        int numpairs = 0;
+        while(1){
+            cin >> l >> r;
+            if(l==0 && r==0) break;
+            if(r<0 || l>m) continue;
+            all[numpairs] = {l, r};
+            numpairs++;
+        }    
+        sort(all, all + numpairs);
         int cnt = 0, currentX = 0, currentPair = 0;
-        
-        while(currentX < m && currentPair < numPairs){
-            solution[cnt].end = 0;
-            for(;allPairs[currentPair].start <= currentX;++currentPair)
-                if (allPairs[currentPair].end > solution[cnt].end)
-                    solution[cnt] = allPairs[currentPair];
-            
-            if (solution[cnt].end == currentX) break;
-            currentX = solution[cnt].end;
-            ++cnt;
+        while(currentX < m && currentPair < numpairs){
+            solution[cnt].second = currentX;
+            while(all[currentPair].first<=currentX && currentPair<numpairs){
+                if(all[currentPair].second > solution[cnt].second){
+                    solution[cnt] = all[currentPair];
+                }
+                currentPair++;
+            }
+            if(solution[cnt].second == currentX) break;
+            currentX = solution[cnt].second;
+            cnt++;
         }
-        
         if(currentX >= m){
-            cout << cnt << endl;
-            for (int i=0;i<cnt;++i){
-                cout << solution[i].start << " " << solution[i].end << endl;
+            cout << cnt << "\n";
+            for(int i=0;i<cnt;++i){
+                cout << solution[i].first << " " << solution[i].second << endl;
             }
         }
-        else cout << "0" << endl;
+        else cout << "0\n";
         if(t!=0) cout << "\n";
     }
     return 0;
