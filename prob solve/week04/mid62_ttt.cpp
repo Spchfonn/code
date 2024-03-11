b4953e34f207eb372819c069bbc0d140e1ad52e4
Spchfonn <57552344+Spchfonn@users.noreply.github.com>
2024-03-11 17:00:16 +0700

    add ttt

--- prob solve/week04/mid62_ttt.cpp
+++ prob solve/week04/mid62_ttt.cpp
@@ -6,42 +6,43 @@ int mark[100010];
 int main(){
     int n, m, u, v, ans = 0;
     cin >> n >> m;
-    while(m--){
-        int i = 0;
+    for(int i=0;i<m;i++){
         cin >> u >> v;
         arr[u].push_back(v);
         arr[v].push_back(u);
-        edge[i++] = make_pair(u, v);
+        edge[i] = make_pair(u, v);
     }
     for(int k=0;k<m;k++){
-        q.push_back(1);
-        mark[1] = 1;
-        while(!q.empty()){
-            int i = q.front();
-            for(auto j:arr[i]){
-                if(mark[j]==0){
-                    q.push_back(j);
-                    mark[j] = -mark[i];
+        ans = 0;
+        for(int l=1;l<=n;l++){
+            if(mark[l]!=0) continue;
+            q.push_back(l);
+            mark[l] = 1;
+            while(!q.empty()){
+                int i = q.front();
+                q.pop_front();
+                for(auto j:arr[i]){
+                    if((j==edge[k].first && i==edge[k].second) || (i==edge[k].first && j==edge[k].second)) continue;
+                    if(mark[i]==mark[j]){
+                        ans = 1;
+                        break;
+                    }
+                    if(mark[j]!=0) continue;
+                    if(mark[j]==0){
+                        q.push_back(j);
+                        mark[j] = -mark[i];
+                    }
                 }
-                else if(mark[i]==mark[j]){
-                    ans = 1;
-                    break;
-                }
+                if(ans==1) break;
             }
             if(ans==1) break;
-            q.pop_front();
         }
         if(ans==0){
             cout << edge[k].first << " " << edge[k].second << "\n";
             break;
         }
-        else if(ans==1) continue;
-        for(int i=1;i<=n;i++){
-            arr[i].clear();
-            mark[i] = 0;
-        }
         if(!q.empty()) q.clear();
-        memset(mark,0,sizeof mark);
+        memset(mark, 0, sizeof mark);
     }
 
     return 0;
