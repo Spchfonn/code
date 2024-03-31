ee68c6faf0e7fe9e2157f821bd28311c9bebab92
Spchfonn <57552344+Spchfonn@users.noreply.github.com>
2024-03-31 22:02:59 +0700

    add wintsch

--- prob solve/week08/wintsch.cpp
+++ prob solve/week08/wintsch.cpp
@@ -1,27 +1,44 @@
 #include<bits/stdc++.h>
 using namespace std;
+int dp[1010], t[1010], w[1010];
+pair<int, int> s[1010];
+vector<int> ans;
 int main(){
-    int n, s, t, cnt = 0;
+    int n, st;
     cin >> n;
-    list<pair<int, int>> l;
-    for(int i=0;i<n;i++){
-        cin >> s >> t;
-        l.push_back(make_pair(t,s));
+    for(int i=1;i<=n;i++){
+        cin >> st >> t[i] >> w[i];
+        s[i] = make_pair(st, i);
     }
-    l.sort();
-    auto state = l.front();
-    int last = state.first;
-    cnt++;
-    l.pop_front();
-    while(!l.empty()){
-        auto state = l.front();
-        if(state.second>=last){
-            cnt++;
-            last = state.first;
+    sort(s, s+n);
+    for(int i=1;i<=n;i++){
+        int j;
+        for(j=i-1;j>0;j--){
+            if(t[j]<=s[i].first) break;
         }
-        l.pop_front();
+        // dp[i] = max(dp[i-1], dp[j]+w[s[i].second]);
+        if(dp[j]+w[s[i].second] > dp[i-1]){
+            dp[i] = dp[j]+w[s[i].second];
+            ans.push_back(i);
+        }
+        else dp[i] = dp[i-1];
     }
-    cout << cnt << "\n";
+    // cout << "\n";
+    // for(int i=1;i<=n;i++) cout << dp[i] << " ";
+    cout << "\n";
+    cout << dp[n] << "\n";
+    for(auto i:ans){
+        cout << i << " ";
+    }
+    
     
     return 0;
 }
+
+
+/*
+3
+1 3 2
+2 4 3
+3 5 2
+*/
